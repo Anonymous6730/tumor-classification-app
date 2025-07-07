b321737614402f584aa99f65957f4777fd999e81
Anonymous6730 <theg1473@gmail.com>
2025-07-07 23:59:17 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,7 +1,7 @@
 import gdown, pickle, streamlit as st
 
 def load_model():
-    url = "https://drive.google.com/uc?id=Prob_Poly_SVC.pkl"
+    url = "https://drive.google.com/uc?export=download&id=1YJaveRTqbWjB-G4dqXm8lDuFGm9Gs4QB"
     gdown.download(url, output="Prob_Poly_SVC.pkl", quiet=True)
     with open("Prob_Poly_SVC.pkl", "rb") as f:
         return pickle.load(f)
