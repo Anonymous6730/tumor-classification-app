85961fd99e0ec62f91e5fca85b544e491181e403
Anonymous6730 <theg1473@gmail.com>
2025-07-07 23:40:59 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -10,6 +10,6 @@ def load_model():
         gdown.download(url, model_path, quiet=False)
     
     with open(model_path, "rb") as f:
-        model = pickle.load(f)
+        model = pickle.loads(f)
     
     return model
