30a8fd3796e166607af0db81cf22c8ed4bdd105b
Anonymous6730 <theg1473@gmail.com>
2025-07-08 21:06:50 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -4,9 +4,9 @@ import pickle
 import streamlit as st
 
 def load_model():
-    model_path = "Prob_Poly_SVC.pkl"
+    model_path = "brain_tumor_cnn.keras"
     if not os.path.exists(model_path):
-        url = "https://drive.google.com/file/d/1YJaveRTqbWjB-G4dqXm8lDuFGm9Gs4QB/view?usp=drive_link"
+        url = "https://drive.google.com/uc?export=download&id=1eZ7v4dQzZ-5JEXi9KFFzDTOzI7w2z_VZ"
         gdown.download(url, model_path, quiet=False)
     
     with open(model_path, "rb") as f:
