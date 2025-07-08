93d2891aa9b8c23b6d19c229e6e5e907e50841f7
Anonymous6730 <theg1473@gmail.com>
2025-07-08 21:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,13 @@ st.set_page_config(
 )
 
 # ---- LOAD CNN MODEL ----
-with st.spinner("Loading model..."):
-    model = load_model()
-st.success("✅ Model loaded successfully!")
+import gdown
+from tensorflow import keras
+
+url = "https://drive.google.com/uc?export=download&id=1eZ7v4dQzZ-5JEXi9KFFzDTOzI7w2z_VZ"
+output = 'brain_tumor_cnn.keras'
+gdown.download(url, output, quiet=False)
+model = keras.models.load_model('brain_tumor_cnn.keras')
 
 # ---- SIDEBAR ----
 with st.sidebar:
